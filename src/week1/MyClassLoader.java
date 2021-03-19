04229103c56970317115471fb86d438ec4865dfd
bosco <hikari77@users.noreply.github.com>
2021-03-19 16:56:58 -0400

    week1 home work

--- src/week1/MyClassLoader.java
+++ src/week1/MyClassLoader.java
@@ -1,4 +1,26 @@
 package week1;
 
-public class MyClassLoader {
+import java.lang.reflect.Method;
+import java.util.Base64;
+
+public class MyClassLoader extends ClassLoader{
+
+    public static void main(String[] args) throws Exception{
+        Object helloInstance = new MyClassLoader().findClass("Hello").getDeclaredConstructor().newInstance();
+        Method helloMethod = helloInstance.getClass().getMethod("hello");
+        helloMethod.invoke(helloInstance); // will print:  Hello, classLoader!
+    }
+
+    @Override
+    protected  Class<?> findClass(String name) throws ClassNotFoundException {
+        String base = "yv66vgAAADQAHAoABgAOCQAPABAIABEKABIAEwcAFAcAFQEABjxpbml0PgEAAygpVgEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBAAVoZWxsbwEAClNvdXJjZUZpbGUBAApIZWxsby5qYXZhDAAHAAgHABYMABcAGAEAE0hlbGxvLCBjbGFzc0xvYWRlciEHABkMABoAGwEABUhlbGxvAQAQamF2YS9sYW5nL09iamVjdAEAEGphdmEvbGFuZy9TeXN0ZW0BAANvdXQBABVMamF2YS9pby9QcmludFN0cmVhbTsBABNqYXZhL2lvL1ByaW50U3RyZWFtAQAHcHJpbnRsbgEAFShMamF2YS9sYW5nL1N0cmluZzspVgAhAAUABgAAAAAAAgABAAcACAABAAkAAAAdAAEAAQAAAAUqtwABsQAAAAEACgAAAAYAAQAAAAEAAQALAAgAAQAJAAAAJQACAAEAAAAJsgACEgO2AASxAAAAAQAKAAAACgACAAAABAAIAAUAAQAMAAAAAgAN";
+        byte[] bytes = decode(base);
+        return defineClass(name, bytes, 0, bytes.length);
+    }
+
+    public byte[] decode(String base64) {
+        return Base64.getDecoder().decode(base64);
+    }
+
+
 }
